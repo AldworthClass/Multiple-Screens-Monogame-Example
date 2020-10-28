c48d4b72aa9ae81f26152751abffc0189c2804b9
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-10-28 13:11:40 -0400

    Add project files.

---
+++ Multiple Screens Example/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Multiple_Screens_Example
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        SpriteFont titleFont;
+        Screen currentLevel;
+
+        enum Screen
+        {
+            Intro,
+            LevelOne,
+            End
+        }
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            currentLevel = Screen.Intro;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            titleFont = Content.Load<SpriteFont>("TitleText");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            if(currentLevel == Screen.Intro)
+            {
+                _spriteBatch.Begin();
+
+                _spriteBatch.DrawString(titleFont, "This is the Intro.  Hit ENTER to play!", );
+                _spriteBatch.End();
+            }
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
